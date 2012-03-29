d41342ace39a563ef367cce764d0c45936e49888
Don Vince <bucanero@gmail.com>
2012-03-30 00:04:12 +0100

    added game loop, seemed to work successfully until near the end of the game when I got the error SystemStackError: stack level too deep

--- hotseat-ruby/pal.rb
+++ hotseat-ruby/pal.rb
@@ -7,7 +7,10 @@ class Atom
     @owner = " "
   end
   
-  def add_neutron
+  def add_neutron(player)
+    return false if @owner != " " and @owner != player
+    
+    @owner = player
     @neutrons += 1
     
     if @neutrons == @neighbours.count
@@ -17,12 +20,13 @@ class Atom
         @owner = " "
       end
     end
+    return true
   end
   
   def disperse_neutrons
     @neighbours.each do |neighbour|
        neighbour.owner = @owner
-       neighbour.add_neutron
+       neighbour.add_neutron(@owner)
     end
   end
 end
@@ -32,8 +36,8 @@ class Grid2d
   
   def initialize(width, height)
     @width = width
-	@height = height
-	@grid = Array.new(width * height) { Atom.new }
+	  @height = height
+	  @grid = Array.new(width * height) { Atom.new }
 	
     for x in 0..width - 1
 	  for y in 0..height - 1
@@ -54,6 +58,11 @@ class Grid2d
     return nil if x >= @width or y >= @height or x < 0 or y < 0
     return @grid[x + y * width]
   end
+  
+  def has_winner?
+    first_owner  = self[0, 0].owner
+    @grid.all? {|i| i.owner != " " and i.owner == first_owner}
+  end
 end
 
 def show_grid(grid)
@@ -68,7 +77,38 @@ def show_grid(grid)
   end
 end
 
+def start_game
+  puts "Player 1?"
+  player_one = gets.chomp
+  puts "Player 2?"
+  player_two = gets.chomp
+  
+  puts "Width?"
+  width = gets.chomp.to_i
+  puts "Height?"
+  height = gets.chomp.to_i
 
+  game_map = Grid2d.new(width, height)
+  
+  player_ones_move = true
+  
+  until game_map.has_winner? do
+    show_grid(game_map)
+    current_player = player_ones_move ? player_one : player_two
+    puts current_player
+    puts "x?"
+    x = gets.chomp.to_i
+    puts "y?"
+    y = gets.chomp.to_i
+    
+    if game_map[x, y].add_neutron(current_player)
+      player_ones_move = !player_ones_move
+    else
+      puts "Invalid move, try again!"
+    end
+    
+  end
+end
 
 
 
